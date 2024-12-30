15e45cf76374294b4b4575653d70a4929b7f3b43
joeperpetua <qwerty.perpetua@gmail.com>
2024-12-30 12:48:53 +0100

    change to static build

--- vite.config.ts
+++ vite.config.ts
@@ -11,22 +11,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  build: {
-    lib: {
-      entry: 'src/main.ts', // Your app's entry point
-      name: 'ControllerVue',
-      fileName: 'controller-vue',
-    },
-    rollupOptions: {
-      external: ['vue'], // Prevent bundling Vue (use parent Vue)
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-        exports: 'default',
-      },
-    },
-  },
+  base: '/embedded/controller-vue/',
   server: {
     https: {
       key: fs.readFileSync('C:\\Windows\\System32\\cert.key'),
